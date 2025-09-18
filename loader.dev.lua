8afa8ca093eb9f26aedee6c1258b361d4689ed9f
rzbz <121601325+rzbz@users.noreply.github.com>
2025-09-18 11:20:43 +0800

    Create loader.dev.lua

---
+++ loader.dev.lua
@@ -0,0 +1,13 @@
+local v1 = "v1"
+local v2 = "v2"
+local f1 = function()
+  print("f1")
+end
+local function f2()
+  print("f2")
+end
+gv1 = "gv1"
+function gf1()
+  print("gf1")
+end
+local o = {"a", "b", "c"}
